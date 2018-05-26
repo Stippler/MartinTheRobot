67af08a174ff1bd74682812ac013a500f5356b9e
kathariemer <riemer.katharina@outlook.com>
2018-05-26 21:07:33 +0200

    asdf

--- Geometry.hs
+++ Geometry.hs
@@ -5,6 +5,8 @@ module Geometry
  , moveY
  , intersects
  , intersectsList
+ , fallingWithGravity
+ , differenceVector
 ) where
 import Graphics.UI.WX.Types
 
@@ -13,7 +15,33 @@ data Circle = Circle {
     , getY :: Int
     , getRadius :: Int
 } deriving (Show)
+      
+data Vec = Vec {
+      xx :: Int
+    , yy :: Int
+} deriving (Show)
 
+data CircleVec = CircleVec {
+      getCircle :: Circle
+    , getVec :: Vec
+}
+
+tslf=10
+
+addV :: Vec -> Vec -> Vec
+addV (Vec x1 y1) (Vec x2 y2) = Vec (x1+x2) (y1+y2)
+
+scalV :: Vec -> Int -> Vec
+scalV (Vec x1 y1) scalar = Vec (x1*scalar) (y1*scalar)
+
+move :: CircleVec -> CircleVec
+move CircleVec (Circle x y r) (Vec vx vy) = CircleVec (Circle (x+vx) (y+vy)) (Vec vx vy)
+
+moveAcc :: CircleVec -> CircleVec
+moveAcc CircleVec circle vec=move (CircleVec circle (gravity vec)) 
+
+gravity :: Vec -> Vec
+gravity Vec x y = Vec x (y+1)
 
 toCircle :: Int -> Point2 Int  -> Circle
 toCircle radius point = Circle (pointX point) (pointY point) radius
@@ -30,4 +58,36 @@ intersectsList circles c1 = any (intersects c1) circles
 distance² :: Circle -> Circle -> Int
 distance² c1 c2 = (getX c1 - getX c2)^2 + (getY c1 - getY c2)^2
 
+-- SIGNATUR VERÄNDERN ?
+fallingWithGravity :: Circle -> Int -> Int -> Circle
+fallingWithGravity (Circle x y r) t0 t = Circle x (y+5*(t-t0)^2) r
+
+differenceVector :: Circle -> Circle -> Point2 Int
+differenceVector c1 c2 = Point (getX c1 - getX c2) (getY c1 - getY c2) 
+
 -- 
+rebound :: Circle -> Circle -> Point2 Int
+rebound shot drop = changeDir velDrop (-1)*alpha -- + "fallverhalten" des regentropfens
+	where d = differenceVector drop shot
+	  alpha = angle (differenceVector shot drop) drop 
+	      -- velDrop = velocity of raindrop
+
+dot :: Point2 Int -> Point2 Int -> Int
+dot p1 p2 = (pointX p1 * pointX p2) + (pointY p1 * pointY p2)
+
+-- Signatur wird nicht passen
+angle :: Point2 Int -> Point2 Int -> Float
+angle v1 v2 = acos(dot v1 v2 / (sqrt $ dot v1 v1)*(sqrt $ dot v2 v2))
+
+changeDir :: Point2 Int -> Float -> Point2 Int
+changeDir (Point x y) alpha = Point (x*cos(alpha) + y*sin(alpha)) (-x*sin(alpha) + y*cos(alpha))
+
+
+
+
+
+
+
+
+
+
