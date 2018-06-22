7adc5b8c77c1548f855a4d1872167c9ecbc20191
kathariemer <riemer.katharina@outlook.com>
2018-06-22 23:27:43 +0200

    ...

--- Object.hs
+++ Object.hs
@@ -36,7 +36,7 @@ shotRadius = 25
 shotSpeed = Vec 0 (-5) 
 martinRadius = 15 
 dropAcc=0.1
-bgSpeed=3
+bgSpeed=5
 dropRadius=50
 
 ------------
@@ -51,7 +51,6 @@ initialMartin = Circle 0 0 0
 updateMartin :: Point2 Int  -> Martin
 updateMartin point = Circle (fromIntegral (pointX point)) (fromIntegral (pointY point)) martinRadius
 
-
 intersectsMartin :: Drops -> Martin -> Bool
 intersectsMartin drops martin = intersectsList drops martin
 
@@ -62,15 +61,20 @@ intersectsMartin drops martin = intersectsList drops martin
 type Shot = CircleVec
 type Shots = [Shot]
 
+-- | appends a shot at (x, y), the center of c, to the beginning of the list of shots s 
 addShot :: Circle -> Shots -> Shots
 addShot c s = (generateShot (c^.x) (c^.y)):s
 
+-- | generates a shot at point (x, y)
 generateShot :: Float -> Float -> Shot
 generateShot x y = CircleVec (Circle x y shotRadius) shotSpeed
 
+-- | moves shots according to their motion vectors and 
 updateShots ::  Drops -> Shots -> Shots
 updateShots drops shots =  filterShots (map move shots) $ drops
 
+-- | removes any shot from shots = [shot] which intersect with any drop from drops = [drop]
+-- |                                   or which have left the frame, their y-coordinate being too small
 filterShots :: Shots -> Drops -> Shots
 filterShots shots drops = filter (not . ( (||) <$> (intersectsList drops)._circle <*> (\ c -> c^.y < (- c^.r))._circle )) shots
 
@@ -82,39 +86,31 @@ type Drop = CircleVec
 type Drops = [Drop]
 
 initialDrops :: Drops
-initialDrops = [--CircleVec (Circle 10 20 64) (Vec 0 0),
-                --CircleVec (Circle 60 00 64) (Vec 0 0),
-                --CircleVec (Circle 110 (-20) 64) (Vec 0 0),
-                CircleVec (Circle 160 (40) 64) (Vec 0 0)]
+initialDrops = [CircleVec (Circle 160 (40) 64) (Vec 0 0)]
 
+-- | appends a new drop at (x = random) just above the frame
 addDrop :: Float -> Drops -> Drops 
 addDrop random drops = generateDrop random : drops
 
+-- | generates a new drop at a (x = random*width) just above the frame
+-- TODO maybe 2 randoms
 generateDrop :: Float -> CircleVec
 generateDrop random = CircleVec (Circle (random*Geometry.width) (-dropRadius) $ dropRadius*random) (Vec 0 0) 
 
+-- | moves all drops and changes their direction, if they intersect with any shot in shots
 updateDrops :: Shots -> Drops -> Drops
 updateDrops shots drops = iterates (map (moveAcc dropAcc) drops) shots collisionOccured
 
-collisionOccured :: Drop -> Martin -> Drop
+-- | changes motion vector of cv based on difference vector between cv's and c's center
+collisionOccured :: Drop -> Circle -> Drop
 collisionOccured cv c = cv & Geometry.vec %~ (addV $ (normed v) `scalV` 5)
             where v = distVec (cv^.Geometry.circle) c
-
----------------------
--- Drops and Shots --
----------------------
-
-updateDropShotPair :: (Shots, Drops) -> (Shots, Drops)
-updateDropShotPair (shots, drops) = reboundShotDropPair (map move shots, map (moveAcc dropAcc) drops) collisionOccured
-
--- removes shots which intersect with a drop and rebounds the drops
-reboundShotDropPair :: (Shots, Drops) -> (CircleVec -> Circle -> CircleVec) -> (Shots, Drops)
-reboundShotDropPair (shots, drops) func =  (filter (not . ( (||) <$> (intersectsList drops)._circle <*> (\ c -> c^.y < (- c^.r))._circle )) shots, iterates drops shots func)
 
 ----------------
 -- background --
 ----------------
 
+-- | updates the background according to the specified speed, i. e. how fast the background moves
 updateBackground :: (Int, Int) -> (Int, Int)
 updateBackground (pos, bgCount) = if pos<round (Geometry.height*2) then (pos+bgSpeed,bgCount) else (0, bgCount+1)
 
@@ -130,6 +126,9 @@ shotImage = bitmap $ "shotSquare.png"
 martinImage = bitmap $ "m2r21.png"
 bgImage = bitmap $ "background3.png"
 
+-- | renders image, where
+-- |                martin, shots and drops ... TODO fixme
+-- |                bgPos ... TODO fixme
 render :: Martin -> Shots -> Drops -> (Int,Int) -> Bool -> DC a -> Rect -> IO ()
 render martin shots drops bgPos shooting dc viewArea = do
   renderBackground dc bgPos
@@ -140,16 +139,19 @@ render martin shots drops bgPos shooting dc viewArea = do
   mapM ((renderDrop dc)) drops
   return ()
 
+-- | ???
 renderCircle :: DC a -> Geometry.Circle -> IO ()
 renderCircle dc c = do
   Graphics.UI.WX.circle dc (point (round $ c^.x) (round $ c^.y)) (round $ c^.r) []
 
+-- | draw background image
 renderBackground :: DC a -> (Int,Int) -> IO ()
 renderBackground dc pos = do 
   drawBitmap dc bgImage (Point 0 $ fst pos) True []
   drawBitmap dc bgImage (Point 0 $ fst pos - round Geometry.height*2) True []
   return ()
 
+-- | 
 renderShot :: DC a -> Shot -> IO ()
 renderShot dc shot = do
   scaleDC dc (shot^.Geometry.circle)
